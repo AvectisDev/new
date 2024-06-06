b2673d8eaae09acf9b3f3604ad7130425b4631b4
KondratenkoAlexey <alexeykondratenko8@gmail.com>
2024-06-06 10:04:24 +0300

    comserv

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 call .venv\Scripts\activate.bat
-cd C:\Users\akondratenko\OneDrive - AVECTIS CJSC\01. Projects\11. Пинская ГНС\04. Development\01. Software\04. WEB_RFID\GNS
-python manage.py runserver
+cd D:\WEB_RFID\GNS
+python manage.py runserver 10.10.12.253:8000
 
 timeout 100
